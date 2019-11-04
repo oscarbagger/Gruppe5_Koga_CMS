2ef7d83bf74e6509b27fd72c61da977a4628e2a0
oscar <oscarbagger@live.dk>
2019-11-04 12:05:09 +0100

    load in footer

--- contentLoad.js
+++ contentLoad.js
@@ -1,11 +1,16 @@
 window.addEventListener("DOMContentLoaded",Start);
 
 let cykler;
+let content;
 const url="https://oscarbagger.dk/kea/Gruppe5_Koga_CMS/wordpress/wp-json/wp/v2/posts"
+const footerUrl="footer.html";
+
+const footer=document.querySelector("footer");
 
 function Start()
 {
-   GetJson(); 
+    GetJson(); 
+    GetHtml(footerUrl);
 }
 
 
@@ -14,4 +19,12 @@ async function GetJson()
     const response= await fetch(url);
     cykler= await response.json();
     console.log(cykler);
+}
+
+async function GetHtml(myUrl)
+{
+    const response= await fetch(myUrl);
+    content= await response.text();
+    footer.innerHTML=content;
+    console.log(content);
 }
