057ebb32e8ffbcdc8e4b0a0fe4091bbe71e56e51
oscar <oscarbagger@live.dk>
2019-11-07 09:40:44 +0100

    footer update

--- contentLoad.js
+++ contentLoad.js
@@ -3,7 +3,10 @@ window.addEventListener("DOMContentLoaded",Start);
 let content;
 const footerUrl="footer.html";
 const navUrl="navigation.html";
+const pageURl="https://oscarbagger.dk/kea/Gruppe5_Koga_CMS/wordpress/wp-json/wp/v2/pages/?&per_page=99";
 
+
+const main=document.querySelector("main");
 const footer=document.querySelector("footer");
 const header=document.querySelector("header");
 
@@ -18,4 +21,10 @@ async function GetHtml(myUrl, contentDest)
     const response= await fetch(myUrl);
     content= await response.text();
     contentDest.innerHTML=content;
+}
+
+async function GetJson()
+{
+    const jsonData= await fetch(pageURl);
+    pages= await jsonData.json();
 }
