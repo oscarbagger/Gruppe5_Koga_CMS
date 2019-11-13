c299dd92b677b6fb0eef037df1dc79e19cb48fd5
1284rosa <1284rosa@stud.kea.dk>
2019-11-13 14:48:35 +0100

    forside stylet, mangler testimonials og lidt mere styling

--- contentLoad.js
+++ contentLoad.js
@@ -1,19 +1,18 @@
-window.addEventListener("DOMContentLoaded",StartLoad);
+window.addEventListener("DOMContentLoaded", StartLoad);
 
 let content;
-const footerUrl="footer.html";
-const navUrl="navigation.html";
-const pageURl="https://oscarbagger.dk/kea/Gruppe5_Koga_CMS/wordpress/wp-json/wp/v2/pages/?&per_page=99";
+const footerUrl = "footer.html";
+const navUrl = "navigation.html";
+const pageURl = "https://oscarbagger.dk/kea/Gruppe5_Koga_CMS/wordpress/wp-json/wp/v2/pages/?&per_page=99";
 
 
-const main=document.querySelector("main");
-const footer=document.querySelector("footer");
-const header=document.querySelector("header");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+const header = document.querySelector("header");
 
-let menuIsOpen=false;
+let menuIsOpen = false;
 
-function StartLoad()
-{ 
+function StartLoad() {
     GetNav();
     GetFooter();
     // check if there exists a class saying that content needs to be loaded in.
@@ -22,50 +21,43 @@ function StartLoad()
     }
 }
 
-async function GetNav()
-{
-    const htmlResponse= await fetch(navUrl);
-    content= await htmlResponse.text();
-    header.innerHTML=content;
-    let nav=header.querySelector(".menuItems");
-    let menu=header.querySelector(".burgermenu");
+async function GetNav() {
+    const htmlResponse = await fetch(navUrl);
+    content = await htmlResponse.text();
+    header.innerHTML = content;
+    let nav = header.querySelector(".menuItems");
+    let menu = header.querySelector(".burgermenu");
     menu.addEventListener("click", () => {
-        if(menuIsOpen==false)
-            {
-                menuIsOpen=true;
-                nav.style.display="flex";
-                menu.querySelector("img").src=src="images/burger_close.png";
-            }
-        else {
-            menuIsOpen=false;
-            nav.style.display="none";
-            menu.querySelector("img").src=src="images/iconmonstr-menu-1.svg";
+        if (menuIsOpen == false) {
+            menuIsOpen = true;
+            nav.style.display = "flex";
+            menu.querySelector("img").src = src = "images/burger_close.png";
+        } else {
+            menuIsOpen = false;
+            nav.style.display = "none";
+            menu.querySelector("img").src = src = "images/iconmonstr-menu-1.svg";
         }
     })
 }
 
-async function GetFooter()
-{
-    const htmlResponse= await fetch(footerUrl);
-    content= await htmlResponse.text();
-    footer.innerHTML=content;
+async function GetFooter() {
+    const htmlResponse = await fetch(footerUrl);
+    content = await htmlResponse.text();
+    footer.innerHTML = content;
 }
 
-async function GetJsonContent()
-{
-    const jsonData= await fetch(pageURl);
-    pages= await jsonData.json();
+async function GetJsonContent() {
+    const jsonData = await fetch(pageURl);
+    pages = await jsonData.json();
     InsertContent();
 }
 
-function InsertContent()
-{
-    let pageText=document.querySelector(".loadedPageContent").textContent;
+function InsertContent() {
+    let pageText = document.querySelector(".loadedPageContent").textContent;
     pages.forEach(page => {
-        if (page.slug==pageText)
-            {
-                main.innerHTML=page.content.rendered+main.innerHTML;
-                console.log("loaded");
-            }
+        if (page.slug == pageText) {
+            main.innerHTML = page.content.rendered + main.innerHTML;
+            console.log("loaded");
+        }
     })
 }
