380d807f019c0d7df4d96d9432bc24c6f95305a6
oscar <oscarbagger@live.dk>
2019-11-18 12:04:43 +0100

    Update listView.js

--- listView.js
+++ listView.js
@@ -1,17 +1,15 @@
 window.addEventListener("DOMContentLoaded", Start);
 
-let kategoriArray = [];
-let urlParams = new URLSearchParams(window.location.search);
-let kategoriID = urlParams.get("id");
-let kategoriUrl = "https://oscarbagger.dk/kea/Gruppe5_Koga_CMS/wordpress/wp-json/wp/v2/kategori?include[]=" + kategoriID;
-let kategoriIndhold = document.querySelector(".kategoriIndhold");
-
-
 let cykler = [];
 const url = "https://oscarbagger.dk/kea/Gruppe5_Koga_CMS/wordpress/wp-json/wp/v2/cykel/?&per_page=99";
 const temp = document.querySelector("template");
 const list = document.querySelector(".list");
 
+let kategoriArray = [];
+let urlParams = new URLSearchParams(window.location.search);
+let kategoriID = urlParams.get("id");
+let kategoriUrl = "https://oscarbagger.dk/kea/Gruppe5_Koga_CMS/wordpress/wp-json/wp/v2/kategori?include[]=" + kategoriID;
+let kategoriIndhold = document.querySelector(".kategoriIndhold");
 
 
 function Start() {
@@ -21,20 +19,20 @@ function Start() {
 async function kategoriGetJson() {
     const response = await fetch(kategoriUrl);
     kategoriArray = await response.json();
+    console.log(kategoriArray);
     kategoriArray = kategoriArray[0];
     showKategori();
 }
-
-async function GetJson() {
-    const response = await fetch(url);
-    cykler = await response.json();
-    ShowList();
-}
-
 function showKategori() {
     kategoriIndhold.innerHTML = kategoriArray.content.rendered;
     myCategory = kategoriArray.title.rendered;
-    GetJson();
+    cyklerGetJson();
+}
+
+async function cyklerGetJson() {
+    const response = await fetch(url);
+    cykler = await response.json();
+    ShowList();
 }
 
 function ShowList() {
@@ -48,16 +46,12 @@ function ShowList() {
             clone.querySelector(".pris").textContent = "Pris: " + cykel.pris + " kr";
             list.appendChild(clone);
             // eventlistener to go to singleview    
-            /* list.lastElementChild.addEventListener("click", () => {
-                location.href = "cykel.html?id=" + cykel.id;
-            }); */
             list.lastElementChild.querySelector(".single_bike").addEventListener("click", () => {
                 location.href = "cykel.html?id=" + cykel.id;
             });
         }
     })
 }
-
 
 function goBack() {
     window.history.back();
